b74daaf78f076de6b2f690f1e249c2dd18f62396
JohnBlec <pavel_safin1@gmail.com>
2025-02-18 21:24:14 +0300

    app v1.1 fix notif

--- run.py
+++ run.py
@@ -6,9 +6,8 @@ import os
 from dotenv import load_dotenv
 from datetime import datetime
 
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
-from aiogram.filters import Command
 from aiogram.client.default import DefaultBotProperties
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
@@ -38,17 +37,18 @@ async def notification():
     try:
         u_s = await s_p_t()
         for user, pay_term in u_s:
-            if pay_term.end_time == datetime.now().date():
-                send_time = f"{pay_term.end_time} 12:00"
+            if pay_term.end_time.date() == datetime.now().date():
+                send_time = f"{pay_term.end_time.date()} 12:00"
                 text = 'Напоминаю, что сегодня нужно продлить подписку на ВПН!'
-
                 send_datetime = datetime.strptime(send_time, '%Y-%m-%d %H:%M')
-
                 scheduler.add_job(
                     send_scheduled_message,
                     trigger=DateTrigger(run_date=send_datetime),
                     args=[user.tg_id, text, pay_term.id]
                 )
+
+                logging.info(f"Уведомление установленно в {send_datetime} для {user.tg_id} подписки {pay_term.id}")
+        logging.info(f"{u_s}")
     except ValueError as v:
         logging.error(f"Ошибка в обработчике: {v}")
     except Exception as e:
